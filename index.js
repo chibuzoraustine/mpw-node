baa2f05cf0e0ecb64e5fbb0b96669acd5e1e5441
Anurigwo Chibuzor <cibuzoraustine50@gmail.com>
2023-03-30 23:52:44 +0100

    Added more methods

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 
+import Authentication from "./lib/authentication.js";
+import Base from "./lib/base.js";
+import Blockchain from "./lib/blockchain.js";
+import Exchange from "./lib/exchange.js";
+import Fiat from "./lib/fiat.js";
 import Misc from "./lib/misc.js";
+import SettlementAccount from "./lib/settlement_account.js";
+import Token from "./lib/token.js";
 import Verification from "./lib/verification.js";
 
 export default class MoiPayWay {
 
-    #public_key;
     #secret_token;
+    #base;
     misc;
     verification;
+    settlementAccount;
+    blockchain;
+    fiat;
+    exchange;
+    token;
 
-    constructor(public_key, secret_token) {
+    constructor(secret_token) {
 
-        this.#public_key = public_key;
         this.#secret_token = secret_token;
-        
-        this.misc = new Misc(this.#secret_token);
-        this.verification = new Verification(this.#secret_token);
-    }   
+        this.#base = new Base(this.#secret_token)
 
+        this.misc = new Misc(this.#base);
+        this.verification = new Verification(this.#base);
+        this.settlementAccount = new SettlementAccount(this.#base);
+        this.blockchain = new Blockchain(this.#base);
+        this.fiat = new Fiat(this.#base);
+        this.exchange = new Exchange(this.#base);
+        this.token = new Token(this.#base);
+    }
+
+}
+
+export async function MoiPayWayAuth(public_key, payload) {
+
+    const _auth = new Authentication(public_key)
+    return await _auth.init(payload);
 
 }
--- services/http-service.js
+++ services/http-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export function http(base_url, token) {
+export default function http(base_url, token) {
     return axios.create({
         baseURL: base_url,
         headers: {
